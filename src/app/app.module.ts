9755584bfa92f5553124360bfb42569a0e7f40d1
abdul_mannan <abdul.mannan@alrawdah.in>
2022-01-22 15:59:10 +0530

    new componnent by mannan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MannanComponent } from './mannan/mannan.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MannanComponent
   ],
   imports: [
     BrowserModule,
